ed9d3b73dc5bb333385da5fda6bbbef6d8c1415e
Sacha <sacha.arbonel@hotmail.fr>
2018-04-25 14:17:18 +0200

    Update handler.py

--- handler.py
+++ handler.py
@@ -19,24 +19,7 @@ def main(event, context):
     array = np.arange(event_a).reshape(event_b, event_c)
     l = array.tolist()
     
-    return {'statusCode': 201,
-                'body': l }
-        
-    # if event.get('httpMethod') == 'POST' and event.get('body'):
-    #     logger.info('Message received')
-    #     data = json.loads(event.get('body'))
-    # data = json.loads(event['body'])
-    #if 'a' not in data or 'b' not in data:
-    #     logging.error('Validation Failed')
-    #     return {'statusCode': 422,
-    #             'body': json.dumps({'error_message': 'Couldn\'t find a nor b'})}
-
-    # if not data['text']:
-    #     logging.error('Validation Failed - a or b was empty. %s', data)
-    #     return {'statusCode': 422,
-    #             'body': json.dumps({'error_message': 'Couldn\'t create the array item. As a or b was empty.'})}
-    
-
+    return {'statusCode': 201, 'body': l }
 
 if __name__ == "__main__":
     main('', '')
